4e5fab72223b18ea699ea77791368fc9a9b428c6
vladikuss <vreshetoff@gmail.com>
2021-09-06 14:25:19 +0500

    feat: add js code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() { }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      return Math.ceil((this.min + this.max) / 2);
     }
-
+  
     lower() {
-
+      this.max = this.guess();
     }
-
+  
     greater() {
-
+      this.min = this.guess();
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
+  
